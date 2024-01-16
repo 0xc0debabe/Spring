521866aaa32388c3b2c03630b57be8d47b2ba40d
0xc0debabe <kongminoo@naver.com>
2024-01-16 22:18:30 +0900

    commitTest

--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,6 +3,8 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 깃 커밋 테스트
+
 @SpringBootApplication
 public class CoreApplication {
 
