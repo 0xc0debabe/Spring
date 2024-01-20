6ed3a7984dd1d792f658b16a9990cfd497268ad1
0xc0debabe <kongminoo@naver.com>
2024-01-20 12:58:49 +0900

    240120_Spring

---
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppCOnfigTest.java
@@ -0,0 +1,33 @@
+package hello.core.scan.filter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ComponentFilterAppCOnfigTest {
+
+    @Test
+    void filterScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        Assertions.assertThat(beanA).isNotNull();
+
+        assertThrows(
+                NoSuchBeanDefinitionException.class,
+        () -> ac.getBean("beanB", BeanB.class));
+    }
+
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = @ComponentScan.Filter(classes = MyIncludeComponent.class),
+            excludeFilters = @ComponentScan.Filter(classes = MyExcludeComponent.class)
+
+    )
+    static class ComponentFilterAppConfig {
+    }
+
+}
